4a06d540cddfbb521285d9ea98ed9ac7ec2b1a09
PEMMANA VISWESHWAR REDDY <143106948+visweshwarreddypemmana@users.noreply.github.com>
2026-03-17 19:10:44 +0530

    Update SQL_Joins.sql

--- SQL-Joins/SQL_Joins.sql
+++ SQL-Joins/SQL_Joins.sql
@@ -1,1 +1,38 @@
+SELECT *
+FROM EMPLOYEE e, DEPARTMENT d
+WHERE e.dept_id = d.dept_id;
 
+
+SELECT *
+FROM EMPLOYEE e, DEPARTMENT d
+WHERE e.dept_id = d.dept_id(+);
+
+
+SELECT *
+FROM EMPLOYEE e, DEPARTMENT d
+WHERE e.dept_id(+) = d.dept_id;
+
+
+SELECT *
+FROM EMPLOYEE e, DEPARTMENT d
+WHERE e.dept_id = d.dept_id(+)
+
+UNION
+
+SELECT *
+FROM EMPLOYEE e, DEPARTMENT d
+WHERE e.dept_id(+) = d.dept_id;
+
+
+SELECT *
+FROM EMPLOYEE e, DEPARTMENT d
+WHERE e.salary > 50000;
+
+
+SELECT *
+FROM EMPLOYEE e1, EMPLOYEE e2
+WHERE e1.manager_id = e2.emp_id;
+
+
+SELECT *
+FROM EMPLOYEE e, DEPARTMENT d;
